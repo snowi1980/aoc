ec142ba53ee0edda3950f64e2149e6576297eaad
Stefan Knaus <stefan.ka.knaus@bmw.de>
2021-11-14 23:25:36 +0100

    Puzzle2 Part2

--- 2020/app/src/main/scala/org/snowi/aoc2020/puzzle2/Puzzle2.scala
+++ 2020/app/src/main/scala/org/snowi/aoc2020/puzzle2/Puzzle2.scala
@@ -1,17 +1,15 @@
 /* (C) stefan.devops@gmail.com 2021 */
 package org.snowi.aoc2020.puzzle2
 
-import java.util.regex.Pattern
-
 object Puzzle2 {
   def main(args: Array[String]): Unit = {
     println("This is puzzle 2")
-    var correctEntries = checkPwEntries(readPwFile("puzzle2/input.txt"))
+    var correctEntries = checkPwEntriesV2(readPwFile("puzzle2/input.txt"))
     println("Result puzzle 2: " + correctEntries)
 
   }
 
-  def checkPw(pwEntry: String): Boolean = {
+  def checkPolicy1(pwEntry: String): Boolean = {
     val tokens = pwEntry.replace("-", " ").split(" ")
     val pw = tokens(3)
     val lower = tokens(0).toInt
@@ -29,6 +27,20 @@ object Puzzle2 {
     return lower <= matches && matches <= upper
   }
 
+  def checkPolicy2(pwEntry: String): Boolean = {
+    val tokens = pwEntry.replace("-", " ").split(" ")
+    val pw = tokens(3)
+    val lower = tokens(0).toInt - 1
+    val upper = tokens(1).toInt - 1
+    val c = tokens(2).replace(":", "").charAt(0)
+    var i = 0
+    var matches = 0
+
+    return (pw.charAt(lower) == c && pw.charAt(upper) != c) || (pw.charAt(
+      lower
+    ) != c && pw.charAt(upper) == c)
+  }
+
   def readPwFile(filename: String): List[String] = {
     val bufferedSource = io.Source.fromResource(filename)
     val lines = (for (line <- bufferedSource.getLines()) yield line).toList
@@ -38,7 +50,17 @@ object Puzzle2 {
 
   def checkPwEntries(entries: List[String]): Int = {
     var correct = 0
-    for (pwEntry <- entries if checkPw(pwEntry)) {
+    for (pwEntry <- entries if checkPolicy1(pwEntry)) {
+      correct += 1
+      println(pwEntry)
+    }
+
+    correct
+  }
+
+  def checkPwEntriesV2(entries: List[String]): Int = {
+    var correct = 0
+    for (pwEntry <- entries if checkPolicy2(pwEntry)) {
       correct += 1
       println(pwEntry)
     }
--- 2020/app/src/test/scala/org/snowi/aoc2020/puzzle2/Puzzle2Suite.scala
+++ 2020/app/src/test/scala/org/snowi/aoc2020/puzzle2/Puzzle2Suite.scala
@@ -8,8 +8,16 @@ import org.scalatestplus.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class Puzzle2Suite extends AnyFunSuite {
 
-  var input = List(
+  var inputV1 = List(
     "1-3 a: abcde",
+    "1-3 b: cdefg",
+    "2-9 c: ccccccccc",
+    "15-19 k: kkkkkkkkkkkkzkkkkkkk"
+  )
+
+  var inputV2 = List(
+    "1-3 a: abcde",
+    "1-3 a: cbade",
     "1-3 b: cdefg",
     "2-9 c: ccccccccc",
     "15-19 k: kkkkkkkkkkkkzkkkkkkk"
@@ -19,17 +27,33 @@ class Puzzle2Suite extends AnyFunSuite {
     // assert(Puzzle1.answer(expenseReport) == 514579)
   }
 
-  test("valid PW") {
-    assert(Puzzle2.checkPw("1-3 a: abcde"))
-    assert(Puzzle2.checkPw("2-9 c: ccccccccc"))
-    assert(Puzzle2.checkPw("15-19 k: kkkkkkkkkkkkzkkkkkkk"))
+  test("valid PW for policy v1") {
+    assert(Puzzle2.checkPolicy1("1-3 a: abcde"))
+    assert(Puzzle2.checkPolicy1("2-9 c: ccccccccc"))
+    assert(Puzzle2.checkPolicy1("15-19 k: kkkkkkkkkkkkzkkkkkkk"))
+  }
+
+  test("valid PW for policy v2") {
+    assert(Puzzle2.checkPolicy2("1-3 a: abcde"))
+    assert(Puzzle2.checkPolicy2("1-3 a: cbade"))
+
   }
 
   test("corrupted PW") {
-    assert(!Puzzle2.checkPw("1-3 b: cdefg"))
+    assert(!Puzzle2.checkPolicy1("1-3 b: cdefg"))
   }
 
-  test("check entries") {
-    assert(Puzzle2.checkPwEntries(input) == 3)
+  test("corrupted PW for policy v2") {
+    assert(!Puzzle2.checkPolicy2("1-3 b: cdefg"))
+    assert(!Puzzle2.checkPolicy2("2-9 c: ccccccccc"))
+    assert(!Puzzle2.checkPolicy2("15-19 k: kkkkkkkkkkkkzkkkkkkk"))
+  }
+
+  test("check entries V1") {
+    assert(Puzzle2.checkPwEntries(inputV1) == 3)
+  }
+
+  test("check entries v2") {
+    assert(Puzzle2.checkPwEntriesV2(inputV2) == 2)
   }
 }
