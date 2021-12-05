feabe041da763d589192e1085dabdf2d2f96169f
Stefan Knaus <stefan.ka.knaus@bmw.de>
2021-12-05 08:07:55 +0100

    Setup AOC 2021

---
+++ 2021/app/src/main/java/org/snowi/aoc2021/puzzle1/Puzzle1.java
@@ -0,0 +1,23 @@
+package org.snowi.aoc2021.puzzle1;
+
+import org.snowi.aoc.core.Puzzle;
+
+import java.util.List;
+
+public class Puzzle1 extends Puzzle {
+
+
+    public Puzzle1(String inputFile) {
+        super(inputFile);
+    }
+
+    @Override
+    public String answer1(List<String> input) {
+        return null;
+    }
+
+    @Override
+    public String answer2(List<String> input) {
+        return null;
+    }
+}
